7067165aaebebe55518150ad83ae4f3ad9ed0d2f
Montassar <montassar.rebhi@fst.utm.tn>
2024-08-22 14:43:31 +0100

    fecthing data from DB using fecth API

--- client/src/contexts/TodoContext.jsx
+++ client/src/contexts/TodoContext.jsx
@@ -1,14 +1,32 @@
 import { createContext, useState, useEffect } from "react";
+const URL = "http://localhost:3000/api/tasks";
 
 export const TasksContext = createContext();
+
 export const TodoProvider = ({ children }) => {
-  const [todos, setTodos] = useState(() => {
-    const storedTodos = localStorage.getItem("todos");
-    return storedTodos ? JSON.parse(storedTodos) : [];
-  });
+  const [todos, setTodos] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchTasks = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(URL);
+      if (!response.ok) {
+        throw new Error("failed to fecth tasks");
+      }
+      const tasks = await response.json();
+      setTodos(tasks.data);
+      console.log(tasks);
+    } catch (error) {
+      console.error(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos]);
+    fetchTasks();
+  }, []);
   return (
     <TasksContext.Provider value={{ todos, setTodos }}>
       {children}
